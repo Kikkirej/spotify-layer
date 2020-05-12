aec14b7c38a4865519d19a19b31a1cc14ebb304b
Fabian Lemke <fabian.lemke@online.de>
2020-05-12 17:34:43 +0200

    :tada: Inital commit

---
+++ src/main/kotlin/net/kikkirej/spotify/layer/SpotifyLayerApplication.kt
@@ -0,0 +1,11 @@
+package net.kikkirej.spotify.layer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpotifyLayerApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpotifyLayerApplication>(*args)
+}
---
+++ src/test/kotlin/net/kikkirej/spotify/layer/SpotifyLayerApplicationTests.kt
@@ -0,0 +1,13 @@
+package net.kikkirej.spotify.layer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpotifyLayerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
